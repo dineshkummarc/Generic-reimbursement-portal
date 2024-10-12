f1b1f227eafe8b93440599f62f57e2067f8c2e15
Dinesh Kumar <mapmydestination@gmail.com>
2024-10-12 20:57:01 +0530

    updated

---
+++ portal using php and mysql/iwp_project/iwp/hospital.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <title>Hospital</title>
+</head>
+<body class="left-sidebar is-preload">
+                <div id="page-wrapper">
+        
+                    <!-- Header -->
+                        <div id="header">
+        
+                            <!-- Inner -->
+                                <div class="inner">
+                                    <header>
+                                        <h1><a href="#" id="logo">Claim your Hospital payment.</a></h1>
+                                    </header>
+                                </div>
+                         </div>
+        
+                    <!-- Main -->
+                        <div class="wrapper style1">
+        
+                            <div class="container">
+                                <div class="row gtr-200">
+                                    <div class="col-4 col-12-mobile" id="sidebar">
+                                        <hr class="first" />
+                                        <section>
+                                            <header>
+                                                <h3><a href="#">Apply  for reimbursement</a></h3>
+                                            </header>
+                                            <p>
+                                                Apply for the medical reimbursement and get your money back just 
+                                                by entering the transaction id.
+
+                                            </p>
+                                            <footer>
+                                                    <form method="POST" action="Apply.php">
+                                                            <button type="submit">Apply</button>
+                                                    </form>
+                                            </footer>
+                                        </section>
+                                       
+                                    </div>
+                                    <div class="col-8 col-12-mobile imp-mobile" id="content">
+                                        <article id="main">
+                                            <header>
+                                                <h2><a href="#">Pay back</a></h2>
+                                                <p>
+                                                    Get your payment after a month </p>
+                                            </header>
+                                            <a href="#" class="image featured"><img src="images/hospital.jpg" alt="" /></a>
+                                        </article>
+                                    </div>
+                                </div>
+    
+    
+
+    <br><br>
+   
+            <script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+</body>
+</html>
